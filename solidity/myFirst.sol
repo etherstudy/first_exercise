a573bfd2a35db55817c2a88a763c3ec18490f62b
goblinok <goblinok@gmail.com>
2018-06-15 11:11:02 +0900

    init

--- solidity/myFirst.sol
+++ solidity/myFirst.sol
@@ -9,23 +9,21 @@ contract myFirst {
         uint age
     );
 
-    function setVal(string _name, uint _age) public {
-        //--------------------------
-        // please enter the set code
-        //--------------------------
-        ////name = _name;
-        ////age = _age;
-
-        //------------------------------------
-        // please enter the trigger-event code
-        //------------------------------------
-        ////First(name, age);
-    }
-
     function getVal() view public returns (string, uint) {
         //--------------------------
-        // please enter the get code
+        // 1. please enter the get code
         //--------------------------
-        ////return (name, age);
+
+    }
+
+    function setVal(string _name, uint _age) public {
+        //--------------------------
+        // 2. please enter the set code
+        //--------------------------
+
+        //------------------------------------
+        // 3. please enter the trigger-event code
+        //------------------------------------
+
     }
 }
